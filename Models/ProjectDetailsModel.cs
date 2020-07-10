3498c60828a594283da5aed89efbbcdc6262d2f8
RaginiMalhotra <RaginiMalhotra@DESKTOP-T12Q553>
2020-07-10 15:27:31 +1000

    Add project files.

---
+++ Models/ProjectDetailsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocsOnline.Models
+{
+    public class ProjectDetailsModel
+    {
+        public SelectList Projectlist { get; set; }
+     
+        public int? ProjectID { get; set; }
+        public string ProjectName { get; set; }
+    }
+}
