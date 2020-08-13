edf2910e5ee327ee4936c1faa1b9f542afd813c9
KalyanandaVedula <bharatvedula04@gmail.com>
2020-08-14 09:13:20 +1000

    tree view

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -7,13 +7,16 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace DocsOnline.Controllers
 {
     public class ProjectsController : Controller
     {
         public static readonly string Filepath = ConfigurationManager.AppSettings["Filepath"];
-
+        public TreeViewModel node = null;
+        public int counter = 1;
+     
         public ActionResult Index()
         {
             if (!UserRoles.UserEditCompleteBookings())
@@ -22,33 +25,90 @@ namespace DocsOnline.Controllers
             }
                 var Projects = new ProjectDetailsModel();
                 Projects.Projectlist = new SelectList(DropDownHelperService.ProjectList().Result, "ID", "Value");
-                IEnumerable<string> dirList = Directory.EnumerateDirectories(Filepath);
-                foreach (string dir in dirList)
+            IEnumerable<string> dirList = Directory.EnumerateDirectories(Filepath);
+            foreach (string dir in dirList)
+            {
+                DirectoryInfo d = new DirectoryInfo(dir);
+
+                string[] files = Directory.GetFiles(Filepath);
+                for (int i = 0; i < files.Length; i++)
                 {
-                    DirectoryInfo d = new DirectoryInfo(dir);
-
-                    string[] files = Directory.GetFiles(Filepath);
-                    for (int i = 0; i < files.Length; i++)
-                    {
-                        files[i] = Path.GetFileName(files[i]);
-                    }
-                    ViewBag.Files = files;
-                    FilesModel dirModel = new FilesModel
-                    {
-                        FileName = Path.GetFileName(dir),
-                        FileDate = d.LastAccessTime,
-                        //FileSize = d.
-                    };
-
-                    // Projects.Folders.Add(d.Name.ToString());
-                    Projects.FolderName.Add(d.Name);
-                    Projects.FolderDate.Add(d.LastWriteTime.Date);
-
+                    files[i] = Path.GetFileName(files[i]);
                 }
-            
+                ViewBag.Files = files;
+                FilesModel dirModel = new FilesModel
+                {
+                    FileName = Path.GetFileName(dir),
+                    FileDate = d.LastAccessTime,
+                    //FileSize = d.
+                };
+
+                // Projects.Folders.Add(d.Name.ToString());
+                Projects.FolderName.Add(d.Name);
+                Projects.FolderDate.Add(d.LastWriteTime.Date);
+
+            }
+
             return View(Projects);
         }
+        private void ConstructTree(DirectoryInfo[] dirList,int id,int pId,bool isParent,TreeViewModel parent,string FilePathRoot)
+        {                       
+            if (dirList == null)
+            {
+                DirectoryInfo di = new DirectoryInfo(FilePathRoot);
+                dirList = di.GetDirectories();
+                id = 1;
+                pId = 0;
+               
+            }
+            string folderPath;
+            DirectoryInfo folder = null;
+            if (parent == null)
+            {
+                folderPath = FilePathRoot;
+                folder = new DirectoryInfo(folderPath);
+            }
+            else
+            {
+                folderPath = parent.name + folder.Name + "/";
+            }
 
+            node = new TreeViewModel(folder.Name);
+            node.children = new List<TreeViewModel>();
+            foreach (var subFolder in folder.GetDirectories())
+            {
+                TreeViewModel child = GetNode(subFolder, node,FilePathRoot);
+               
+                node.children.Add(child);
+            }
+
+        }
+
+        private TreeViewModel GetNode(DirectoryInfo folder, TreeViewModel parent,string FilepathRoot)
+        {
+            string folderPath;
+
+            if (parent == null)
+            {
+                folderPath = FilepathRoot;
+                folder = new DirectoryInfo(folderPath);
+            }
+            else
+            {
+                folderPath = parent.name + folder.Name + "/";
+            }
+
+            TreeViewModel node = new TreeViewModel(folder.Name);
+            node.children = new List<TreeViewModel>();
+            foreach (var subFolder in folder.GetDirectories())
+            {
+                TreeViewModel child = GetNode(subFolder, node,FilepathRoot);
+               
+                node.children.Add(child);
+            }
+
+            return node;
+        }
         public ActionResult Upload()
         {
             return View();
@@ -89,6 +149,7 @@ namespace DocsOnline.Controllers
         public ActionResult GetListOfFiles(string FolderName)
         {
             var _filepath = System.IO.Path.Combine(Filepath, FolderName);
+            //Filepath = _filepath;
             DirectoryInfo d = new DirectoryInfo(_filepath);
             List<FilesModel> files = new List<FilesModel>();
             foreach (FileInfo info in d.GetFiles())
@@ -103,9 +164,8 @@ namespace DocsOnline.Controllers
             };
                 files.Add(fileModel);
         }
-
-            //   return View();
-            return new JsonResult { Data = files, JsonRequestBehavior = JsonRequestBehavior.AllowGet }; ;
+            ConstructTree(null, 1, 0, true,null,_filepath);
+            return new JsonResult { Data = node, JsonRequestBehavior = JsonRequestBehavior.AllowGet }; ;
         }
     }
 }
---
+++ Models/TreeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocsOnline.Models
+{
+    public class TreeViewModel
+    {
+        public TreeViewModel(string theName)
+        {
+            name = theName;
+        }
+        public string name { get; set; }
+        public bool open { get; set; }
+        public bool isParent { get; set; }
+
+        public List<TreeViewModel> children { get; set; }
+
+    }
+}
